ae43293253108dad7a7a4826d76b1a4236ae6027
Daniel <dani@Danis-MacBook-Pro.local>
2020-05-06 00:07:09 +0300

    Added postgressql env and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from db import db
@@ -8,7 +9,8 @@ from security import authentication, identity
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'daniel'
 api = Api(app)
